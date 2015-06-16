0f4bc93b84b9d18dd4f6a03747b7226fce95d2d2
Julie Shi <younghajulie@gmail.com>
2015-06-15 21:54:50 -0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('.standing').show();
+	$('.ready').hide();
+	$('.throwing').hide();
+	$('.cool').hide();
+	$('.hadouken').hide();
+
+	$('.audio-intro').get(0).play();
+});
