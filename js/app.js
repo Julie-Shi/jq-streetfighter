e2b3ad9d48113a56b3687d1e0dc813691fccb8b0
Julie Shi <younghajulie@gmail.com>
2015-06-16 17:53:05 -0700

    Second commit. added more jQuery

--- js/app.js
+++ js/app.js
@@ -4,6 +4,34 @@ $(document).ready(function() {
 	$('.throwing').hide();
 	$('.cool').hide();
 	$('.hadouken').hide();
-
 	$('.audio-intro').get(0).play();
+
+
+
+	//mouse hovers on Ryu 
+
+	$('.ryu').mouseenter(function() {
+		$('.standing').hide();
+		$('.ready').show();
+	});
+
+	$('.ryu').mouseleave(function() {
+		$('.ready').hide();
+		$('.standing').show();
+	});
+
+
+
+	//click on Ryu for "Hadouken"
+
+	$('.ryu').click(function() {
+		$('.ready').hide();
+		$('.standing').hide();
+		$('.throwing').show();
+		$('.hadouken').show();
+
+		
+
+	});
+
 });
